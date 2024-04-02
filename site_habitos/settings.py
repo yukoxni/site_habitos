d22911aece269b25f024a4ef29ad5fc9e09e00e1
samjs <135473303+saramjs@users.noreply.github.com>
2024-04-02 12:25:25 -0300

    tela de login
    uma tela de login simples

--- site_habitos/settings.py
+++ site_habitos/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'site_habitos.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')], #essa linha
         'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
